c453410784b7e5ddbd5c6279a31b4f38efaece0c
Alex Fedorenko <alexandrfedorenko94@gmail.com>
2018-02-23 21:28:08 +0200

    add display image by link from project A

--- app/src/main/java/com/example/dizzer/testtaskprojectb/activity/MainActivity.java
+++ app/src/main/java/com/example/dizzer/testtaskprojectb/activity/MainActivity.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.dizzer.testtaskprojectb.R;
 import com.example.dizzer.testtaskprojectb.constants.CustomConstants;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +18,51 @@ public class MainActivity extends AppCompatActivity {
     private Intent incomeIntent;
     private int followedIntent;
 
+    private ImageView imageView;
+    private String imageLink;
+    private int statusForLink = CustomConstants.STATUS_UNKNOWN;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = (ImageView) findViewById(R.id.iv_main_activity);
+
         incomeIntent = getIntent();
         followedIntent = incomeIntent.getIntExtra(CustomConstants.INTENT_CODE, CustomConstants.LAUNCHER);
+        imageLink = incomeIntent.getStringExtra(CustomConstants.INTENT_LINK);
 
-        if (followedIntent == CustomConstants.LAUNCHER){
-            closeApp();
+        switch (followedIntent) {
+            case CustomConstants.LAUNCHER:
+                closeApp();
+                break;
+            case CustomConstants.TEST_FRAGMENT:
+                showImage();
+                break;
+            case CustomConstants.HISTORY_FRAGMENT:
+                break;
         }
+    }
+
+    private void showImage(){
+        Picasso.with(MainActivity.this).load(imageLink).into(imageView, new Callback() {
+            @Override
+            public void onSuccess() {
+                statusForLink = CustomConstants.STATUS_DOWNLOADED;
+                saveLink();
+            }
+
+            @Override
+            public void onError() {
+                statusForLink = CustomConstants.STATUS_ERROR;
+                saveLink();
+            }
+        });
+    }
+
+    private void saveLink() {
+        //TODO Realize save to DB
     }
 
     private void closeApp() {
